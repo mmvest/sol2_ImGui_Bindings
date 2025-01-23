aac82d78770ccc681893f2c73738e6ea1f21a8c2
mmvest <53498772+mmvest@users.noreply.github.com>
2025-01-22 21:29:55 -0600

    Fixed ImGui::SameLine binding missing an override

--- sol_ImGui.h
+++ sol_ImGui.h
@@ -2743,7 +2743,8 @@ namespace sol_ImGui
 		ImGui.set_function("Separator"						, Separator);
 		ImGui.set_function("SameLine"						, sol::overload(
 																sol::resolve<void()>(SameLine), 
-																sol::resolve<void(float)>(SameLine)
+																sol::resolve<void(float)>(SameLine),
+																sol::resolve<void(float, float)>(SameLine)
 															));
 		ImGui.set_function("NewLine"						, NewLine);
 		ImGui.set_function("Spacing"						, Spacing);
